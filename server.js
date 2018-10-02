4e0d15545cb43e0a1cad8e8d60e4d2bc35dd2ddb
haris tanvir <haristanvir@hariss-MacBook-Pro.local>
2018-10-02 00:07:57 -0230

    assignment1

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
+
+var server = express();
+
+var projects = [];
+
+//middlewares
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.set('view engine', 'ejs');
+server.set('views',path.join(__dirname,'views'))
+server.get ('/', function(req,res){
+    if (Object.keys(req.query).length !== 0){
+        projects.push(req.query)
+    }
+    res.render("index",{
+        projects:projects
+    });
+});
+
+server.get ('/clear', function(req,res){
+    projects = [];
+    res.redirect('/');
+});
+
+server.listen(3000,function(){
+console.log("server has started");
+});
