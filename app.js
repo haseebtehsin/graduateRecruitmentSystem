c67bab771e1253fabee7d6c3605bbffc96ee3891
haris00 <haristanvir@hotmail.com>
2018-11-28 14:45:01 -0330

    attempt to solve heroku routes problem

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
